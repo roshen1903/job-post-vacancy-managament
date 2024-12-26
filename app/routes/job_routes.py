50aad76da6acf21a456dbf5adfdbb41df6aa7ab6
roshen1903 <rovsenbekirov@icloud.com>
2024-12-26 09:10:36 +0400

    Update job_routes.py

--- app/routes/job_routes.py
+++ app/routes/job_routes.py
@@ -1,10 +1,3 @@
 from flask import Flask
 
-app = Flask(_name_)
-
-@app.route('/')
-def home():
-    return 'Welcome to the Inventory Management System!'
-
-if _name_ == '_main_':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+app = Flask(_name)  # Use __name, not _name
