e5272d6232bd8210ad5d85e2f6c13c552125b576
roshen1903 <rovsenbekirov@icloud.com>
2024-12-26 09:29:16 +0400

    Update job_routes.py

--- app/routes/job_routes.py
+++ app/routes/job_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 
 # Define the Blueprint correctly using _name_
-job_bp = Blueprint('job_bp', _name_)
+job_bp = Blueprint('job_bp', __name__)
 
 @job_bp.route('/jobs', methods=['GET'])
 def get_jobs():
