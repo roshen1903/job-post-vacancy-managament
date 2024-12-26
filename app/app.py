3fd9a447e5120cced39e808df9133fd4a6875183
roshen1903 <rovsenbekirov@icloud.com>
2024-12-26 09:34:34 +0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,5 +6,6 @@ app = Flask(_name_)
 def home():
     return 'Welcome to the Inventory Management System!'
 
-if _name_ == '_main_':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if _name_ == "_main_":
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
